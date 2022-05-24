c479d14c590bb5b293f23c163788d483db55d01d
Maxime-Moilliet <mllt.dev66@gmail.com>
2022-05-24 10:26:23 +0200

    add: init starter

---
+++ main.js
@@ -0,0 +1,9 @@
+class ImageZoom {
+  constructor(imageId, resultId) {
+    this.image = document.getElementById(imageId);
+    this.result = document.getElementById(resultId);
+    // Code
+  }
+}
+
+new ImageZoom("image", "result");
